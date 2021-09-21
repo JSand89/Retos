702d14f938e0d0bfe8cab5eba9fc9b71c60d6272
JSand89 <javier.esand@gmail.com>
2021-09-20 21:40:17 -0500

    reto anmation scroll

--- ScrolllAnimation/script.js
+++ ScrolllAnimation/script.js
@@ -1,19 +1,24 @@
 const boxes = document.querySelectorAll('.box')
+//console.log(boxes)
 
 window.addEventListener('scroll', checkBoxes)
 
-checkBoxes()
+//checkBoxes()
 
 function checkBoxes() {
-    
     console.log(window.innerHeight / 5 * 4)
-    
-    const triggerBottom = window.innerHeight / 5 * 4
-    console.log(triggerBottom)
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
-        console.log(boxTop)
+        const triggerBottom = window.innerHeight/ 5 * 4
+        console.log(boxTop,'boxtop',triggerBottom,'triger') 
+        if(triggerBottom>boxTop) {
+       box.classList.add('show')
+       console.log('if')
+       }else{
+           box.classList.remove('show')
+       }
+     
          //usar un if para comprobar donde esta si esta dentro de la ventana si esta cambiarle el estado al .show
         
     })
